665ccea04d535084a26a3e24020827c951df26f2
anitha2962 <anithasivadas92@gmail.com>
2024-02-03 20:16:49 +0530

    'first-commit'

---
+++ METHOD OVERLOADING/Program.cs
@@ -0,0 +1,19 @@
+﻿
+class program
+{
+    static int plusmethodint (int x, int y)
+        {
+        return x + y;
+}
+    static double plusmethoddouble(double x, double y)
+    {
+        return x + y;
+    }
+    static void main(string[] args)
+    {
+        int num1=plusmethodint(1, 2);
+        double mynum2=plusmethoddouble(4.3, 2.7);
+        Console.WriteLine("int:" + num1);
+        Console.WriteLine("double:" + mynum2);
+    }
+}
